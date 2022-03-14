ffde7c75b76a0f17bc13bf19178794def9d39bd4
Santiago Iglesias <69163333+santi4039@users.noreply.github.com>
2022-03-14 09:47:53 -0300

    Core files of meme project in React

---
+++ App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState} from "react";
+import html2canvas from 'html2canvas';
+
+function App() {
+
+  const [linea1, setLinea1] = React.useState('');
+  const [linea2, setLinea2] = React.useState('');
+  const [imagen, setImagen] = React.useState('');
+
+  const onChangeLinea1 = function(evento){
+    setLinea1(evento.target.value)
+  }
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
+  }
+  const onChangeImagen = function(evento){
+    setImagen(evento.target.value)
+  }
+  const onClickExportar = function(evento){
+   
+    html2canvas(document.querySelector("#meme")).then(canvas => { //Funcion para transformar imagen
+      var img = canvas.toDataURL("image/png");
+
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+
+  });
+  }
+
+
+
+
+  return (
+    <div className="App">
+      
+      <select onChange={onChangeImagen}>
+        <option value="fire">Casa en llamas</option>
+        <option value={"futurama"}>Futurama</option>
+        <option value="history">History Channel</option>
+        <option value = "matrix">Matrix</option>
+        <option value ="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select> <br/>
+      
+      <input onChange={onChangeLinea1} type="text" placeholder='Linea 1'/><br/>
+      <input onChange={onChangeLinea2} type="text" placeholder='Linea 2'/><br/>
+
+      <button onClick={onClickExportar}>Exportar</button><br/>
+
+      <div className = "meme" id='meme'>
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span>
+        <img src={'/images/' + imagen + '.jpg'} />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
